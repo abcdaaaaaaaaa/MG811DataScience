18b9ee23e6251465ab7623faef7c4162ccf61bab
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-09-17 00:46:43 +0300

    Update information.ino

---
+++ examples/information/information.ino
@@ -0,0 +1,10 @@
+---------- For Data Science ----------
+  For data science applications: https://github.com/abcdaaaaaaaaa/MQDataScience/tree/main/DataScience
+  For time-based correction coefficient assisted 4d ppm slope estimation in Data Science Applications: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/4D_Slope/4DSlope.py
+  For only read and estimate current ppm values with correction coefficient (does not include 4D axis features) in Data Science Applications: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/4D_Slope/readppm.py
+  For 4D air quality graphical sensor simulation with time-based correction coefficient support in data science applications: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/4D_Surface/4DSurface.py
+  To generate regression curves of all supported MG811 gases: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/Regression/MG811_Curves.py
+
+---------- Library Examples Files ----------
+  To process your SP3S-AQ2-01 into the microcontroller: SP3SAQ2.ino
+  To have general information about all the files in the library: information.ino
