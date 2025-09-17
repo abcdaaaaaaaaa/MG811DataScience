15adcbbbd7fff251d6dc33c19dee178d985d89b3
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-09-17 11:49:16 +0300

    Update information.ino

--- examples/information/information.ino
+++ examples/information/information.ino
@@ -1,9 +1,17 @@
 ---------- For Data Science ----------
   For data science applications: https://github.com/abcdaaaaaaaaa/MQDataScience/tree/main/DataScience
-  For time-based correction coefficient assisted 4d ppm slope estimation in Data Science Applications: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/4D_Slope/4DSlope.py
+  For time-based correction coefficient assisted 4d ppm slope estimation in Data Science Applications: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/4D_Slope/run.py (4DSlope.py)
   For only read and estimate current ppm values with correction coefficient (does not include 4D axis features) in Data Science Applications: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/4D_Slope/readppm.py
   For 4D air quality graphical sensor simulation with time-based correction coefficient support in data science applications: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/4D_Surface/4DSurface.py
   To generate regression curves of all supported MG811 gases: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/Regression/MG811_Curves.py
+  For the time-dependent Resume and Response curve of the correction coefficient: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/Response%20and%20Resume%20Curve/Response%20and%20Resume%20Curve.py
+  For Theoretical CO2 analysis with the Nernst equation: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/TheoreticalCO2/TheoreticalCO2.py
+  For Theoretical CO2 ppm estimation with the Nernst equation: https://github.com/abcdaaaaaaaaa/MG811DataScience/blob/main/DataScience/4D_Slope/TheoreticalCO2.py
+
+--WARNING--
+  For time-based correction coefficient assisted 4d ppm slope estimation and For Theoretical CO2 ppm estimation with the Nernst equation:
+  For a brief ppm estimate, please only run "run.py". The run.py file will run 4DSlope.py and TheoreticalCO2.py in the appropriate order.
+  Please replace 4D_Slope/4D_Datas.xlsx with your actual measured data.
 
 ---------- Library Examples Files ----------
   To process your MG811 into the microcontroller: MG811.ino
